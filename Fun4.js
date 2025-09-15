27fa842e4193e0a01e560311a3bb35636e3f9b64
Itsc <itschokky@gmail.com>
2025-09-16 06:00:27 +0700

    sec

---
+++ Fun4.js
@@ -0,0 +1,32 @@
+function f(x,funcstr){
+    return eval(funcstr);
+}
+function calbi(){
+    let func = document.getElementById("func").value;
+    let xl = parseFloat(document.getElementById("XL").value);
+    let xr = parseFloat(document.getElementById("XR").value);
+    let tol = 0.000001;
+    let fxl = f(xl,func);
+    let fxr = f(xr,func);
+    let mid ;
+    let iter =0;
+    let output="";
+    while ((xr - xl)/2 > tol){
+        iter++;
+        mid = (xl+xr)/2;
+        let fmid =f(mid,func);
+        output+= `${iter},${xl},${xr},${mid},${fmid}\n`;
+        if (fmid==0){
+            break
+        }else if((fxl*fmid)<0){
+            xr = mid;
+            fxr = fmid;
+        }else
+        {
+            xl=mid;
+            fxl=fmid
+        }
+    }
+    output += `${mid}`
+    document.getElementById("result").textContent = output;
+}
